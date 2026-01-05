58e00494f2439109962a8686249e33d7b9dee4e6
Satnam <satnamsingh@vancedsolutions.com>
2026-01-05 18:57:44 +0530

    fix slug

--- src/utils/capitalizeTitle.ts
+++ src/utils/capitalizeTitle.ts
@@ -1,12 +1,11 @@
 export function capitalizeTitle(slug: string): string {
-    if (!slug || typeof slug !== 'string') {
-        return '';
-    }
+  if (!slug || typeof slug !== "string") return "";
 
-    return slug
-        .replace(/_/g, '-')
-        .split('-')
-        .filter(Boolean)
-        ?.map(word => word[0]?.toUpperCase() + word.slice(1))
-        .join(' ');
+  const text = slug
+    .replace(/_/g, "-")
+    .split("-")
+    .filter(Boolean)
+    .join(" ");
+
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
